b8957cfa0f1718fbcb21a0fb692868d7b87ee6a7
mateusmaiamaia <mateusmaiamaia@hotmail.com>
2022-09-13 22:40:23 -0300

    fix: fix knapSack algorithm
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

--- backend/app/structures/functions.py
+++ backend/app/structures/functions.py
@@ -1,6 +1,7 @@
 from cmath import inf
 from platform import node
 from turtle import distance
+from typing import List
 
 
 class Graph: 
@@ -71,17 +72,17 @@ class Graph:
 
         return path  
 
-    def knapSack(W, wt, val, n):
-        K = [[0 for x in range(w + 1)] for x in range(n + 1)]
-        for i in range(n + 1):
-            for w in range(W + 1):
-                if i == 0  or  w == 0:
-                    K[i][w] = 0
-                elif wt[i-1] <= w:
-                    K[i][w] = max(val[i-1]
-                         + K[i-1][w-wt[i-1]],
-                                K[i-1][w])
-                else:
-                    K[i][w] = K[i-1][w]
-        return K[n][W]                 
+    def knapSack(self, W: int, wt: List[int], val:List[int], n: int):
+      K = [[0 for x in range(W + 1)] for x in range(n + 1)] 
+      for i in range(n + 1): 
+          for w in range(W + 1): 
+              if i == 0 or w == 0: 
+                  K[i][w] = 0
+              elif wt[i-1] <= w: 
+                  K[i][w] = max(val[i-1] 
+                            + K[i-1][w-wt[i-1]],   
+                                K[i-1][w]) 
+              else: 
+                  K[i][w] = K[i-1][w] 
+      return K[n][W]                 
                 
